b62ab8887ab89ffe1625c3216a0f834d12ea59dc
grainrigi <naonaokiryu2@gmail.com>
2018-09-14 23:56:16 +0900

    Add mirrorlist editing

--- src/arch_init.sh
+++ src/arch_init.sh
@@ -3,8 +3,33 @@
 # Obtain the absolute path of this script
 SCRIPT_PATH="$(realpath "$0")"
 
-echo -e "\e[36m[ARCH-INIT] Installing fundamental packages...\e[m"
 
+# Setup the mirrorlist
+MIRRORLIST=/etc/pacman.d/mirrorlist
+MIRRORTEMP=/tmp/mirrorlist
+
+echo -e "\e[36m[ARCH-INIT] Rewriting /etc/pacman.d/mirrorlist...\e[m"
+
+# Copy the current mirrorlist into /tmp
+cp $MIRRORLIST $MIRRORTEMP
+
+# Firstly, search for "Japan" to get the mirror URL of Japan
+JAPAN_LINES=$(grep -e "Japan" -n $MIRRORTEMP | sed -e 's/:.*//g')
+
+# Now we get the line numbers of Japan mirror URL.
+# Next, move the URL to the top of the list.
+for line in $JAPAN_LINES;
+do
+    # Delete the original line
+    sed -i ${line}d $MIRRORLIST
+    sed -i ${line}d $MIRRORLIST
+
+    # Copy the URL to the top of the list
+    sed -i 6a"$(sed -n $((${line}+1))p $MIRRORTEMP)" $MIRRORLIST
+    sed -i 6a"$(sed -n $((${line}))p $MIRRORTEMP)" $MIRRORLIST
+done
+
+echo -e "\e[36m[ARCH-INIT] Installing fundamental packages...\e[m"
 yes | pacman -Syu
 yes | pacman -S vim openssh
 
